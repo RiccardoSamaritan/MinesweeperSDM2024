c9c56dcff2eb662af730f3119415c29990aeefb7
Ricky1 <riccardo.samaritan@gmail.com>
2024-12-17 22:26:08 +0100

    Moved testGameOverWhenMineIsRevealed() to GameOverTest

--- src/test/java/CellsTest.java
+++ src/test/java/CellsTest.java
@@ -4,20 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class CellsTest {
 
-    //test 1: game over when revealing a mined cell
-    @Test
-    void testGameOverWhenMineIsRevealed() {
-        Minefield minefield = new Minefield(9, 9, 10);
-        MineSweeper game = new MineSweeper(minefield);
-        game.getGrid()[0][0].setMine(true);
-        boolean hasMine = game.revealCell(0,0);
-        assertAll(
-                ()-> assertTrue(game.getGameOver(), "Game over should be true"),
-                ()-> assertTrue(hasMine, "hasMine should be true")
-        );
-    }
-
-    //test 2: flagged cell cannot be reveled
+    // flagged cell cannot be revealed
     @Test
     void testFlaggedCellCannotBeRevealed() {
         Minefield minefield = new Minefield(9, 9, 10);
--- src/test/java/GameOverTest.java
+++ src/test/java/GameOverTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GameOverTest {
+
+    @Test
+    void testGameOverWhenMineIsRevealed() {
+        Minefield minefield = new Minefield(9, 9, 10);
+        MineSweeper game = new MineSweeper(minefield);
+        game.getGrid()[0][0].setMine(true);
+        boolean hasMine = game.revealCell(0,0);
+        assertAll(
+                ()-> assertTrue(game.getGameOver(), "Game over should be true"),
+                ()-> assertTrue(hasMine, "hasMine should be true")
+        );
+    }
 }
