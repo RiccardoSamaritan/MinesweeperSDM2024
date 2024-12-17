1e6859682151d5e1adae20ec2843b4b059593c46
vice <matteo.vicenzino@gmail.com>
2024-12-17 12:20:02 +0100

    craeted Minefield class

---
+++ src/main/java/Minefield.java
@@ -0,0 +1,12 @@
+public class Minefield {
+    int x;
+    int y;
+    int n_mine;
+
+    Minefield(int x, int y, int n_mine) {
+        this.x = x;
+        this.y = y;
+        this.n_mine = n_mine;
+    }
+
+}
